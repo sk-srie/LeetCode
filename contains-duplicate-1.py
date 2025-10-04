71d591f7ac4ba5637580a96bc600367ed5d9157f
dhanasrie <dhanasrie04@gmail.com>
2025-10-04 13:27:48 +0530

    solved contains-duplicate

---
+++ contains-duplicate-1.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for i in nums:
+            if i in s:
+                return True
+            s.add(i)
+        return False
+
+if __name__  == "__main__":
+    nums = list(map(int, input("Enter space seperated numbers: ").split()))
+    sol = Solution()
+    print(sol.containsDuplicate(nums))
